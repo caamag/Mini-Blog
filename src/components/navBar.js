31bf18121f89ecbc1f23a431ae2df7d2e3d2d50d
Caio Lopes <caiolopesfv@gmail.com>
2024-04-09 20:02:13 -0300

    criando projeto na vercel

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -2,25 +2,41 @@ import './navBar.css';
 
 import { useAuthentication } from '../hooks/useAuthernticate';
 import { useAuthValue } from '../context/authcontext';
+import { useState } from 'react';
 
 import { NavLink } from 'react-router-dom';
 
 function NavBar() {
+
+    const [confirmSignOut, setConfirmSignOut] = useState(false);
 
     const { user } = useAuthValue();
     const { logOut } = useAuthentication();
 
     return <nav>
         <ul>
-            <li><NavLink to='/' className='nav-link'>HOME</NavLink></li>
+            {user && <li><NavLink to='/' className='nav-link'>HOME</NavLink></li>}
             <li><NavLink to='/about' className='nav-link'>ABOUT</NavLink></li>
             {user && <>
                 <li><NavLink to='/post/create' className='nav-link'>NEW POST</NavLink></li>
                 <li><NavLink to='/dashboard' className='nav-link'>DASHBOARD</NavLink></li>
             </>}
             {!user && <li><NavLink to='/login' className='nav-link'>LOGIN</NavLink></li>}
-            {user && <li><button onClick={() => { logOut() }}>SIGN OUT</button></li>}
+            {user && <li><button onClick={() => { setConfirmSignOut(true) }}>SIGN OUT</button></li>}
         </ul>
+
+        {confirmSignOut && <>
+            <div className='logout-confirm'>
+                <h2>Would you like to sign out?</h2>
+                <div>
+                    <button onClick={() => {
+                        logOut()
+                        setConfirmSignOut(false)
+                    }}>SIGN OUT</button>
+                    <button onClick={() => { setConfirmSignOut(false) }}>NO</button>
+                </div>
+            </div>
+        </>}
     </nav>
 
 };
