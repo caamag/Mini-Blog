c79262fc3f9a0ebe53e784cf6c68088c7adf5d43
Caio Lopes <caiolopesfv@gmail.com>
2024-04-09 22:35:20 -0300

    enviando atualizações do about e responsividade de modo geral

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,11 +1,43 @@
 import './about.css'
 
+//imgs imports
+import firebaseIcon from './imgs/firebase-icon.jpeg';
+import reactIcon from './imgs/react-icon.jpeg';
+import gitIcon from './imgs/git-icon.jpeg';
+
+
 
 function About() {
 
     return <div className="container">
 
-        <h1>About page</h1>
+        <section className='content about-title'>
+            <h1>About this <span>Project</span></h1>
+            <p>This project aims at my personal and professional<br />
+                development within the most commonly used web technologies in the market.</p>
+        </section>
+
+        <section className='content about-content'>
+            <div>
+                <h3>FEATURES</h3>
+                <ul>
+                    <li>Create a new account</li>
+                    <li>Sign In/Sign Out</li>
+                    <li>Share new posts</li>
+                    <li>Make comments</li>
+                </ul>
+                <button>REPOSITORY</button>
+            </div>
+
+            <div className='technologies-icons'>
+                <h3>TECHNOLOGIES</h3>
+                <ul>
+                    <li><img src={firebaseIcon} alt="" />Firebase</li>
+                    <li><img src={reactIcon} alt="" />React.js</li>
+                    <li><img src={gitIcon} alt="" />Git</li>
+                </ul>
+            </div>
+        </section>
 
     </div>
 
