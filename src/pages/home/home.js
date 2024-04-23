9cd8ddb0e4be290773165e3b68365c6424b4318a
Caio Lopes <caiolopesfv@gmail.com>
2024-04-23 19:52:53 -0300

    submiting a new hook

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,42 @@
 import './home.css'
 
+//hooks
+import { useNavigate, Link } from 'react-router-dom';
+import { useState } from 'react';
+
+//components
+
 
 function Home() {
 
+    const [query, setQuery] = useState('');
+    const [posts] = useState([])
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
     return <div className="container">
 
-        <h1>Home page</h1>
+        <form onSubmit={handleSubmit}>
+            <input type="text"
+                placeholder='Search by Tags'
+                value={query}
+                onChange={(e) => { setQuery(e.target.value) }} />
+
+            <button>Search</button>
+        </form>
+
+        <div className='posts-container'>
+
+            {posts && posts.length === 0 && (
+                <div className='post-content'>
+                    <h3>No posts made yet...</h3>
+                    <Link to={'/post/create'}>Create a new POST</Link>
+                </div>
+            )}
+
+        </div>
 
     </div>
 };
